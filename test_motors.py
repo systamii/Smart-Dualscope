bdde5c6ee53a1e99d8b8bf8ff96d23dd8a82d5f2
eneri4 <irenehong123@gmail.com>
2025-11-15 15:55:41 -0500

    Integrated pan/tilt motor movement

--- test_motors.py
+++ test_motors.py
@@ -1,35 +1,45 @@
 from time import sleep
 import RPi.GPIO as gpio
 
-direction_pin   = 20
-pulse_pin       = 21
-cw_direction    = 0 
-ccw_direction   = 1 
+pan_direction   = 20
+pan_pulse       = 21
+tilt_direction  = 23 
+tilt_pulse      = 24
+cw_direction    = 0
+ccw_direction   = 1
 
 gpio.setmode(gpio.BCM)
-gpio.setup(direction_pin, gpio.OUT)
-gpio.setup(pulse_pin, gpio.OUT)
-gpio.output(direction_pin,cw_direction)
+gpio.setup(pan_direction, gpio.OUT)
+gpio.setup(pan_pulse, gpio.OUT)
+gpio.setup(tilt_direction, gpio.OUT)
+gpio.setup(tilt_pulse, gpio.OUT)
+
+tilt_steps = 0
 
 try:
     while True:
-        print('Direction CW')
-        sleep(.5)
-        gpio.output(direction_pin,cw_direction)
-        for x in range(200):
-            gpio.output(pulse_pin,gpio.HIGH)
-            sleep(.001)
-            gpio.output(pulse_pin,gpio.LOW)
-            sleep(.0005)
+        # if tilt_steps <= 100:
+        #     print(f'Step: {tilt_steps}')
+        #     sleep(.05)
+        #     gpio.output(tilt_direction,ccw_direction)
+        #     gpio.output(tilt_pulse,gpio.HIGH)
+        #     sleep(.0001)
+        #     gpio.output(tilt_pulse,gpio.LOW)
+        #     sleep(.0001)
+        #     tilt_steps+=1
+        # else:
+        #     pass
+        # Need to implement a push switch so that the camera lens doesn't crash into the pan mechanism
+    
+        sleep(.0001)
+        gpio.output(pan_direction,cw_direction)
+        gpio.output(pan_pulse,gpio.HIGH)
+        sleep(.0001)
+        gpio.output(pan_pulse,gpio.LOW)
+        sleep(.0001)
 
-        print('Direction CCW')
-        sleep(.5)
-        gpio.output(direction_pin,ccw_direction)
-        for x in range(200):
-            gpio.output(pulse_pin,gpio.HIGH)
-            sleep(.001)
-            gpio.output(pulse_pin,gpio.LOW)
-            sleep(.0005)
+
+
 
 except KeyboardInterrupt:
     gpio.cleanup()
